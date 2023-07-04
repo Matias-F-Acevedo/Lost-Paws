24a28a6b1e81565fe37c58cd290728eedef4a40c
Matias F. Acevedo <matias.acevedo56732@gmail.com>
2023-07-03 22:24:56 -0300

    agregue un poco de estilos

--- src/usuarios/login/Login.jsx
+++ src/usuarios/login/Login.jsx
@@ -9,7 +9,7 @@ import { UserContext } from "../../UserContext.jsx";
 function Login() {
 
     // con useContext uso el estado global del UserContext
-    const { user, handleLogin, handleLogout } = useContext(UserContext);
+    const { user, handleLogin} = useContext(UserContext);
 
 
     const [userName, setUserName] = useState("");
@@ -18,7 +18,7 @@ function Login() {
     const [error, setError] = useState("");
     const [users, setUsers] = useState([]);
 
-    const urlUsuariosRegistrados = "https://648e054b2de8d0ea11e87f07.mockapi.io/users"
+    const urlUsersRegistered = "https://648e054b2de8d0ea11e87f07.mockapi.io/users"
 
 
     const fetchUsers = async (url) => {
@@ -33,7 +33,7 @@ function Login() {
 
     // maneja el estado de los usuarios cada vez que se monte el componente
     useEffect(() => {
-        fetchUsers(urlUsuariosRegistrados);
+        fetchUsers(urlUsersRegistered);
     }, []);
 
 
@@ -42,7 +42,7 @@ function Login() {
         event.preventDefault();
 
         if (userName === "" || password === "") {
-            setError("Ingrese todos los campos");
+            setError("Enter all fields");
             return
         } else {
             const user = users.find(
@@ -52,37 +52,12 @@ function Login() {
             if (user) {
                 //guarda el usuario en contexto global y en localstorage
                 handleLogin(user);
-                setError("Loggeado Correctamente");
+                setError("Successfully logged in");
             } else {
-                setError("La contraseña o el usuario que ha ingresado es incorrecto");
+                setError("The password or user you have entered is incorrect.");
             }
         }
     }
-
-
-
-    // const handleSubmit = (event) => {
-    //     event.preventDefault()
-
-    //     if (userName === "" || password === "") {
-    //         setError("Ingrese todos los campos");
-    //         return
-    //     } else {
-
-    //         fetch(urlUsuariosRegistrados).then((response) => response.json()).then((data) => data.forEach(element => {
-    //             if (element.userName === userName && element.password === password) {
-    //                 setAcceso(true)
-    //                 return
-    //             }
-
-    //         })).catch((error) => console.log(error, "No se puedo resolver la peticion"));
-    //     }
-
-    //     if (accesoPermitido === false) {
-    //         setError("La contraseña o el usuario que ha ingresado es incorrecto");
-    //     }
-    // }
-
 
 
 
@@ -90,15 +65,17 @@ function Login() {
         <>
             <Navbar/>
             {user ?
-                <div>
-                    <h1 className="login-welcome">{`Bienvenido ${userName.toLocaleUpperCase()}`}</h1>
+                <div className="container-login-welcome">
+                    <h1 className="login-welcome">{`Welcome ${userName.toLocaleUpperCase()} 🐾`}</h1>
+
                 </div>
+                
                 :
                 <div className="container-login">
                     <div className="login">
                         <h1 className="title-login">Log in</h1>
-                        <p>{error}</p>
-                        <form onSubmit={handleSubmit} className="formulario">
+                        <p className="p-error">{error}</p>
+                        <form onSubmit={handleSubmit} className="form-login">
                             <input type="text" value={userName} placeholder="User" onChange={event => setUserName(event.target.value)} />
                             <input type="password" value={password} placeholder="Password" onChange={event => setPassword(event.target.value)} />
                             <button type="submit">Sing in</button>
